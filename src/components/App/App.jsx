062f45d67f6b5e636ea4ae223acf99e6ad2afdf5
Ruslan <Giovingo92@gmail.com>
2023-06-20 18:31:04 +0300

    did the navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,19 @@
 import { Layout } from 'components/Layout/Layout';
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../../pages/Home'));
+const RegisterPage = lazy(() => import('../../pages/Register'));
+const LoginPage = lazy(() => import('../../pages/Login'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,15 @@
 import { AppBar } from 'components/AppBar/AppBar';
 import { Container } from 'components/Container/Container.styled';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
     <Container>
       <AppBar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
     </Container>
   );
 };
